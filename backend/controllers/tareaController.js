f43207cec5a4129ebe3fcf8f367d9514af57fa57
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-09-13 21:23:44 +0000

    Tareas

---
+++ backend/controllers/tareaController.js
@@ -0,0 +1,15 @@
+import Tarea from "../models/Tareas.js"
+
+const agregarTarea = async (req, res) => {}
+const obtenerTarea = async (req, res) => {}
+const actualizarTarea = async (req, res) => {}
+const eliminarTarea = async (req, res) => {}
+const cambiarEstado = async (req, res) => {}
+
+export {
+    agregarTarea,
+    obtenerTarea,
+    actualizarTarea,
+    eliminarTarea,
+    cambiarEstado
+}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import conectarDB from './config/db.js'
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import proyectoRoutes from './routes/proyectoRoutes.js'
+import tareaRoutes from './routes/tareaRoutes.js'
 
 const app =  express()
 app.use(express.json())
@@ -14,6 +15,7 @@ conectarDB()
 // Routing
 app.use('/api/usuarios', usuarioRoutes)
 app.use('/api/proyectos', proyectoRoutes)
+app.use('/api/tareas', tareaRoutes)
 
 const PORT = process.env.PORT || 4000 
 app.listen(PORT, () => {
