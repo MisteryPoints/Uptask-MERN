4217e753392fac3ccc2e99d9f7aeeb7989cc0ae4
MisteryPoints <misterypoint2@gmail.com>
2022-09-12 23:07:40 -0400

    React Begins

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -17,21 +17,63 @@ const nuevoProyecto = async (req, res) => {
 }
 
 const obtenerProyecto = async (req, res) => {
-    const { id } = req.params
-    const proyecto = await Proyecto.findById(id)
-    console.log(proyecto)
-    if(!proyecto) {
-        return res.status(404).json({ msg: "No Encontrado" }) 
-    } 
-    return res.json(proyecto)
+    const { id } = req.params 
+    try { 
+        const proyecto = await Proyecto.findById(id) 
+        if(proyecto.creador.toString() !== req.usuario._id.toString()){
+            const error = new Error('Acción no valida')
+            return res.status(401).json({ msg: error.message })
+        }
+        return res.json(proyecto) 
+    } catch { 
+        const error = new Error('Proyecto no Encontrado')
+        return res.status(404).json({ msg: error.message })
+    }   
 }
 
 const editarProyecto = async (req, res) => {
+    const { id } = req.params 
+    try { 
+        const proyecto = await Proyecto.findById(id) 
+        if(proyecto.creador.toString() !== req.usuario._id.toString()){
+            const error = new Error('Acción no valida')
+            return res.status(401).json({ msg: error.message })
+        } 
 
+        proyecto.nombre = req.body.nombre || proyecto.nombre
+        proyecto.descripcion = req.body.descripcion || proyecto.descripcion
+        proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega
+        proyecto.cliente = req.body.cliente || proyecto.cliente
+        try {
+            const proyectoAlmacenado = await proyecto.save()
+            res.json(proyectoAlmacenado)
+        } catch (error) {
+            console.log(error)
+        }
+    } catch { 
+        const error = new Error('Proyecto no Encontrado')
+        return res.status(404).json({ msg: error.message })
+    }   
 }
 
 const eliminarProyecto = async (req, res) => {
-
+    const { id } = req.params 
+    try { 
+        const proyecto = await Proyecto.findById(id) 
+        if(proyecto.creador.toString() !== req.usuario._id.toString()){
+            const error = new Error('Acción no valida')
+            return res.status(401).json({ msg: error.message })
+        }
+        try {
+            await proyecto.deleteOne()
+            res.json({msg: 'Proyecto Eliminado Correctamente.'})
+        } catch (error) {
+            console.log(error)
+        } 
+    } catch { 
+        const error = new Error('Proyecto no Encontrado')
+        return res.status(404).json({ msg: error.message })
+    }       
 }
 
 const agregarColaborador = async (req, res) => {
